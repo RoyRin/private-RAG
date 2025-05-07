f4ac8951e6a2c1b9a7822f6f2e243ce1c88595f5
Dennis Du <dennisd1122@gmail.com>
2025-05-07 15:13:46 -0400

    command line arguments for Wikipedia Path ORAM

--- path_ORAM_python/oram_example_client.py
+++ path_ORAM_python/oram_example_client.py
@@ -1,6 +1,7 @@
 import requests
+import sys
 
-query = "Fenway Park"
+query = sys.argv[1]
 
 response = requests.post("http://localhost:8080/read",
                          json={"encrypted_query": query})
--- path_ORAM_python/oram_server.py
+++ path_ORAM_python/oram_server.py
@@ -3,43 +3,46 @@ from pydantic import BaseModel
 import uvicorn
 from wikipedia_path_oram import WikipediaPathORAM
 from contextlib import asynccontextmanager
+import sys
 
 
-# Lifespan-based initialization
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    app.state.wikipedia_path_oram = WikipediaPathORAM(
-        pickle_file='C:/Users/denni/OneDrive/Desktop/Courses/CS 2540/Final Project/private-RAG/path_ORAM_python/wikipedia_path_oram.pkl'
-    ).load_articles(
-        articles_directory='C:/Users/denni/OneDrive/Desktop/Courses/CS 2540/Final Project/example_wiki',
-        database_file_name='C:/Users/denni/OneDrive/Desktop/Courses/CS 2540/Final Project/private-RAG/path_ORAM_python/wikipedia_path_oram.db',
-        bucket_size=4,
-        block_len=4096
-    )
-    yield
+def create_app(articles_directory, pickle_file, database_file_name):
+    # Lifespan-based initialization
+    @asynccontextmanager
+    async def lifespan(app: FastAPI):
+        app.state.wikipedia_path_oram = WikipediaPathORAM(
+            pickle_file=pickle_file
+        ).load_articles(
+            articles_directory=articles_directory,
+            database_file_name=database_file_name,
+            bucket_size=4,
+            block_len=4096
+        )
+        yield
 
+    # Create app with lifespan
+    app = FastAPI(lifespan=lifespan)
 
-# Create app with lifespan
-app = FastAPI(lifespan=lifespan)
+    # Request schema
+    class Query(BaseModel):
+        encrypted_query: str
 
-
-# Request schema
-class Query(BaseModel):
-    encrypted_query: str
-
-
-@app.post("/read")
-async def rag_endpoint(query: Query, request: Request):
-    user_query = query.encrypted_query  # use plaintext
-    try:
-        response = request.app.state.wikipedia_path_oram.read(user_query)
-    except KeyError as key_error:
-        response = {"key_error": str(key_error)}
-    return {"results": response}
+    @app.post("/read")
+    async def rag_endpoint(query: Query, request: Request):
+        user_query = query.encrypted_query  # use plaintext
+        try:
+            response = request.app.state.wikipedia_path_oram.read(user_query)
+        except KeyError as key_error:
+            response = {"key_error": str(key_error)}
+        return {"results": response}
+    
+    return app
 
 
 def main():
-    uvicorn.run("oram_server:app", host="0.0.0.0", port=8080)
+    articles_directory, pickle_file, database_file_name = sys.argv[1:4]
+    app = create_app(articles_directory, pickle_file, database_file_name)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
 
 
 if __name__ == "__main__":
